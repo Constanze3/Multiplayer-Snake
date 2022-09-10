d476567ce87b4e0863df541bb4df682911372e46
Constanze <81444666+Constanze3@users.noreply.github.com>
2022-09-10 08:12:33 +0200

    Update client.js

--- public/client.js
+++ public/client.js
@@ -23,7 +23,7 @@ function setup() {
     ui = new UI();
     gameOverMenu = new GameOverMenu();
 
-    setupSocket(`https://multiplayer-snake-production.up.railway.app:${PORT}`);
+    setupSocket(`https://multiplayer-snake-production.up.railway.app:3000`);
 
     createCanvas(800, 700);
     frameRate(60);
